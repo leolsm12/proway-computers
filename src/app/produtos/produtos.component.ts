bc0ecc56b75ef7ec08002dd22b080616a67e49d4
Leonardo Schaffer <leoschaffer@hotmail.com>
2023-04-28 14:10:47 -0300

    first commit

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { produtos, IProduto } from '../produtos';
+import { ProdutosService } from '../produtos.service';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit {
+  produtos: IProduto [] | undefined = produtos;
+
+  constructor(
+    private produtosService: ProdutosService
+  ) {}
+  ngOnInit(): void {
+      this.produtos = this.produtosService.getAll();
+  }
+  
+}
+console.log(produtos)
